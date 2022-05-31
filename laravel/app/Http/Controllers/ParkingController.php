d29eec383f728d052f861c33fc6d50b2c7ca985c
StephenGit <stephen.stienstra@outlook.com>
2022-05-31 11:32:38 +0100

    log

--- laravel/app/Http/Controllers/ParkingController.php
+++ laravel/app/Http/Controllers/ParkingController.php
@@ -3,13 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ParkingSession;
-
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ParkingController extends Controller
 {
 
     public function HandleReservation(ParkingSession $request){
+
         return redirect('/home')->with('message', 'Uw transactie gegevens');
 
     }
