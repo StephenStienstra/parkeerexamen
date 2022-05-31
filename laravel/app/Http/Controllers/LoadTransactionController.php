bb77c82c9514fc8c8aec87b0f21f08396445d691
Joost Bodewes <97054264@st.deltion.nl>
2022-05-31 14:00:01 +0200

    Transactie joinen met andere database is gelukt

--- laravel/app/Http/Controllers/LoadTransactionController.php
+++ laravel/app/Http/Controllers/LoadTransactionController.php
@@ -12,7 +12,7 @@ use App\Models\ParkingPrices;
 use App\Models\Places;
 use App\Models\Provinces;
 use App\Models\Location;
-use App\Models\Costumers;
+use App\Models\Customer;
 //use Illuminate\Support\Facades\DB;
 
 class LoadTransactionController extends Controller
@@ -25,27 +25,10 @@ class LoadTransactionController extends Controller
 
     public function index(){
 
-        $transactions = Transactions::all();
-        $vmiddel = vmiddel::all();
-        $communities = Communities::all();
-        $parkingprices = ParkingPrices::all();
-        $places = Places::all();
-        $provinces = Provinces::all();
-        $location = Location::all();
-        $customers = Costumers::all();
-        // $test = DB::table('transacties')
-        //    ->join('locaties', 'transacties.ID_Parkeerplaats', '=', 'locaties.ID_Parkeerplaats')
-        //    ->get();
+        $transactions = Transactions::join('locaties', 'transacties.ID_Parkeerplaats', '=', 'locaties.ID_Parkeerplaats')
+           ->get();
 
-        $transactieArray = json_decode($transactions);
-        $locationArray = json_decode($location);
-        $dataArray = array_merge($transactieArray, ['ID_parkeeplaats' => $locationArray]);
-        $data = json_encode($dataArray);
-        //$data = $transactions->merge($location);
-
-
-        //return view('dashboard', compact(['transactions','vmiddel', 'communities', 'parkingprices', 'places', 'provinces', 'location', 'customers']));
-        return view('dashboard', compact(['data']));
+        return view('dashboard', compact(['transactions']));
 
     }
 
--- laravel/resources/views/dashboard.blade.php
+++ laravel/resources/views/dashboard.blade.php
@@ -19,29 +19,15 @@
             </th>
         </tr>
 
-        {{$data}}
-        <!--@/foreach ($data as $transactie)
+    @foreach ($transactions as $transaction)
         <tr>
-            <td>{/{$transactie->ID_Parkeerplaats}}</td>
-            <td>{/{$transactie->kenteken}}</td>
-            <td>{/{$transactie->begintijd}}</td>
-            <td>{/{$transactie->eindtijd}}</td>
-            <td>{/{$transactie->adres}}</td>
-
+            <td>{{$transaction->ID_Parkeerplaats}}</td>
+            <td>{{$transaction->kenteken}}</td>
+            <td>{{$transaction->begintijd}}</td>
+            <td>{{$transaction->eindtijd}}</td>
+            <td>{{$transaction->adres}}</td>
         </tr>
-    @/endforeach
-    @/foreach ($transactions as $transaction)
-        <tr>
-            <td>{/{$transaction->ID_Parkeerplaats}}</td>
-            <td>{/{$transaction->kenteken}}</td>
-            <td>{/{$transaction->begintijd}}</td>
-            <td>{/{$transaction->eindtijd}}</td>
-            @/for(i=0; i<$location.length; i++)
-                @/if($transaction->ID_Parkeerplaats = $location[i]['ID_Parkeeplaats'])
-                    <td>{/{$location[i]['adres']}}</td>
-            @/endfor
-        </tr>
-    @/endforeach-->
+    @endforeach
     </table>
 
 @endsection
